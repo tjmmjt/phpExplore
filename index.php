128a619e476d08f13612cf7e794406903406612c
tjmmjt <tjmmjt@protonmail.com>
2024-04-02 05:58:13 -0500

    added bootstrap before setting up ViewMembers table

--- index.php
+++ index.php
@@ -4,23 +4,20 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home Server</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <header>
-    <div class="headerTitle">
-      <h1>MELSSEN HOME SERVER</h1>
+    <div>
+      <h1><strong>MELSSEN HOME SERVER</strong></h1>
     </div>
 
     <nav class="headerNav">
-      <?php
-      echo ' 
       <a href="/index.php">HOME</a>
       <a href="/pages/AddMember.php">ADD MEMBER</a>
       <a href="/pages/ViewMembers.php">VIEW MEMBERS</a>
-      ';
-      ?>
-      </nav>
+    </nav>
   </header>
 
   <div>
